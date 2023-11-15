6a4771aad1b95c0192caf56cbee513fe2e2351c3
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-11-14 22:19:13 -0300

    mudei quase nada

---
+++ 1.py
@@ -0,0 +1,6 @@
+
+lista = [(1,10), (1,9), (5,1), (3,5)]
+
+lista_aux = sorted(lista)
+print(lista[0][1])
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import database as db
+from database import *
 
 eI = int(input("ESTAÇÃO QUE VOCÊ ESTÁ: ")) - 1      # Estação Inicial
+
 eF = int(input("ESTAÇÃO QUE VOCÊ DESEJA IR: ")) - 1 # Estação Final
