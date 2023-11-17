08abd218ca80ac1e43e28fbac6b4deba5ec39b9d
Vítor Gabriel <vggd@cin.ufpe.br>
2023-11-17 08:27:33 -0300

    add Node

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ from database import *
 eI = int(input("ESTAÇÃO QUE VOCÊ ESTÁ: ")) - 1      # Estação Inicial
 
 eF = int(input("ESTAÇÃO QUE VOCÊ DESEJA IR: ")) - 1 # Estação Final
+
+class Node():
+    #Inicia o nó
+    def __init__(self, parent = None, pos = None):
+        self.parent = parent
+        self.pos = pos
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    #Verifica se os nós são iguais
+    def __eq__(self, other):
+        return self.pos == other.pos
